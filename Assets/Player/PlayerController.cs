8d02f54e04267c14b206a397ada6d467bbc4eff9
yongckim <zxcv9203@naver.com>
2024-01-16 20:34:47 +0900

    :sparkles: 플레이어 점프 기능 추가

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     float axisH = 0.0f;         // 입력
     public float speed = 3.0f;
     
+    public float jump = 9.0f;       // 점프력
+    public LayerMask groundLayer;   // 착지할 수 있는 레이어
+    bool goJump = false;            // 점프 개시 플래그
+    bool onGround = false;          // 지면에 서 있는 플래그
     
     // Start is called before the first frame update
     void Start()
@@ -36,11 +40,46 @@ public class PlayerController : MonoBehaviour
             // 좌우 반전시키기
             transform.localScale = new Vector2(-1, 1);
         }
+        
+        // 캐릭터 점프하기
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
     }
 
     void FixedUpdate()
     {
-        // 속도 갱신하기
-        rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+        // 착지 판정
+        onGround = Physics2D.Linecast(
+            transform.position,
+            transform.position - (transform.up * 0.1f),
+            groundLayer
+        );
+
+        // 지면 위거나 속도가 0이 아님
+        if (onGround || axisH != 0)
+        {
+            // 속도 갱신하기
+            rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+        }
+
+        if (onGround && goJump)
+        {
+            // 지면 위에서 점프 키 눌림
+            // 점프하기
+            Debug.Log("점프");
+            Vector2 jumpPw = new Vector2(0, jump); // 점프를 위한 벡터 생성
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse); // 순간적인 힘 가하기
+            goJump = false; // 점프 플래그 끄기
+        }
+
+    }
+
+    public void Jump()
+    {
+        // 점프 플래그 켜기
+        goJump = true;
+        Debug.Log("점프 버튼 눌림");
     }
 }
