77ebb74117171b99275eed09ea31dfccd374ba5c
yongckim <zxcv9203@naver.com>
2024-01-18 03:36:42 +0900

    :sparkles: 스크롤 화면 적용

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float leftLimit = 0.0f;      // 왼쪽 스크롤 제한
+    public float rightLimit = 0.0f;     // 오른쪽 스크롤 제한
+    public float topLimit = 0.0f;       // 위 스크롤 제한
+    public float bottomLimit = 0.0f;    // 아래 스크롤 제한
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            // 카메라의 좌표 갱신
+            float x = player.transform.position.x;
+            float y = player.transform.position.y;
+            float z = transform.position.z;
+            
+            // 가로 방향 동기화 (양끝에 이동 제한 적용)
+            if (x < leftLimit)
+            {
+                x = leftLimit;
+            }
+            else if (x > rightLimit)
+            {
+                x = rightLimit;
+            }
+            
+            // 세로 방향 동기화 (위아래에 이동 제한 적용)
+            if (y < bottomLimit)
+            {
+                y = bottomLimit;
+            }
+            else if (y > topLimit)
+            {
+                y = topLimit;
+            }
+            
+            // 카메라 위치의 Vector3 만들기
+            Vector3 v3 = new Vector3(x, y, z);
+            transform.position = v3;
+        }
+    }
+}
