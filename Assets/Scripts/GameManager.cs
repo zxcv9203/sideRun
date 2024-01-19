8a60e62154ca7181c1beda779c26dfe112406269
yongckim <zxcv9203@naver.com>
2024-01-19 22:36:03 +0900

    :sparkles: 시간 제한 기능 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject mainImage;    // 이미지를 담아주는 GameObject
-    public Sprite gameOverSpr;      // GAME OVER 이미지
-    public Sprite gameClearSpr;     // GAME CLEAR 이미지
-    public GameObject panel;        // 패널
-    public GameObject restartButton;// RESTART 버튼
-    public GameObject nextButton;   // NEXT 버튼
+    public GameObject mainImage; // 이미지를 담아주는 GameObject
+    public Sprite gameOverSpr; // GAME OVER 이미지
+    public Sprite gameClearSpr; // GAME CLEAR 이미지
+    public GameObject panel; // 패널
+    public GameObject restartButton; // RESTART 버튼
+    public GameObject nextButton; // NEXT 버튼
 
-    Image titleImage;               // 이미지를 표시하는 Image 컴포넌트
+    public GameObject timeBar;
+    public GameObject timeText;
+    TimeController timeCnt;
+
+
+    Image titleImage; // 이미지를 표시하는 Image 컴포넌트
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +27,17 @@ public class GameManager : MonoBehaviour
         Invoke("InactiveImage", 1.0f);
         // 버튼(패널)을 숨기기
         panel.SetActive(false);
+        
+        timeCnt = GetComponent<TimeController>();
+
+        if (timeCnt != null)
+        {
+            if (timeCnt.gameTime == 0.0f)
+            {
+                // 시간 제한이 없으면 시간 표시바 숨김처리
+                timeBar.SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -35,6 +52,10 @@ public class GameManager : MonoBehaviour
             bt.interactable = false;
             mainImage.GetComponent<Image>().sprite = gameClearSpr;
             PlayerController.gameState = "gameend";
+            if (timeCnt != null)
+            {
+                timeCnt.isTimeOver = true; // 시간 카운트 중지
+            }
         }
         else if (PlayerController.gameState == "gameover")
         {
@@ -45,11 +66,34 @@ public class GameManager : MonoBehaviour
             bt.interactable = false;
             mainImage.GetComponent<Image>().sprite = gameOverSpr;
             PlayerController.gameState = "gameend";
+            if (timeCnt != null)
+            {
+                timeCnt.isTimeOver = true; // 시간 카운트 중지
+            }
         }
         else if (PlayerController.gameState == "playing")
         {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            PlayerController playerCnt = player.GetComponent<PlayerController>();
             
+            // 시간 갱신
+            if (timeCnt != null)
+            {
+                if (timeCnt.gameTime > 0.0f)
+                {
+                    // 정수에 할당하여 소수점 이하를 버림
+                    int time = (int)timeCnt.displayTime;
+                    // 시간 갱신
+                    timeText.GetComponent<TextMeshProUGUI>().text = time.ToString();
+                    // 타임 오버
+                    if (time == 0)
+                    {
+                        playerCnt.GameOver();
+                    }
+                }
+            }
         }
+        
     }
 
     void InactiveImage()
